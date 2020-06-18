e278d1a7c1d18bea592da2862eed16f78044e2bb
Aivaras <lukauskasaivaras@gmail.com>
2020-06-18 14:03:22 +0300

    Updates

---
+++ precache-manifest.d8d0aedd5422f0088aa6b88be7d0d938.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aecb99d965ddb4df7a210f4f27afa012",
+    "url": "/yatzy/index.html"
+  },
+  {
+    "revision": "0ced4e13d16b428c8dc5",
+    "url": "/yatzy/static/css/main.d56af3a9.chunk.css"
+  },
+  {
+    "revision": "77adfc6db32d5ff513dc",
+    "url": "/yatzy/static/js/2.1d51cf24.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yatzy/static/js/2.1d51cf24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ced4e13d16b428c8dc5",
+    "url": "/yatzy/static/js/main.87092fdd.chunk.js"
+  },
+  {
+    "revision": "2cb899aa1cf22102f74c",
+    "url": "/yatzy/static/js/runtime-main.5f590b7e.js"
+  },
+  {
+    "revision": "f5a1d338bebd7490b438206e7acdbc95",
+    "url": "/yatzy/static/media/dice-3.f5a1d338.png"
+  },
+  {
+    "revision": "f96884a8bd95307a93a6ba0a239511a7",
+    "url": "/yatzy/static/media/dice-4.f96884a8.png"
+  },
+  {
+    "revision": "a163882ac367f05f8178851d199dd7fe",
+    "url": "/yatzy/static/media/dice-5.a163882a.png"
+  },
+  {
+    "revision": "4fbe0c1921be0e2c2b4477ad2c18c257",
+    "url": "/yatzy/static/media/dice-6.4fbe0c19.png"
+  }
+]);
